58b5757266872119df4055f18f8f0e8d14baf227
QA 1 <QA.1@groupiig.com>
2026-06-25 14:09:55 +0545

    Configured CI

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
 
+
 def get_driver():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     return driver
